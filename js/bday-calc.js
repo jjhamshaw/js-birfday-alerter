32bf97ed1642fdeeb52e794fb0548d186e5a3d35
jon <jonathanhamshaw@gmail.com>
2013-03-27 22:37:41 +0000

    bit of a spring clean

--- js/bday-calc.js
+++ js/bday-calc.js
@@ -4,19 +4,19 @@ function PersonRepo(){
 		new Person("aug", "31st", new Date(1985, 7, 31)),
 		new Person("March(25th)", "Test", new Date(1985, 2, 25)),
 		new Person("jan", "1st", new Date(1985, 0, 1)),
-		new Person("Amy", "Dizzy", new Date(1985, 6, 26), 'http://m3.licdn.com/mpr/pub/image-CXw7gw0aocDJ1uHTW_8hczpU9HeRe4AThu8CQ9UF9e_4Vb_NCXwCQSQa96TExq853t57/amy-dziurzynski.jpg'),
+		new Person("Amy", "Dizzy", new Date(1985, 6, 26)),
 		new Person("dec", "30th", new Date(1985, 11, 30))
 	];
 
-	function Person (firstName, lastName, birthday, imgPath){
-		if (typeof(imgPath)==='undefined') {
-			imgPath = 'http://www.gravatar.com/avatar/22ec28d4cdc9781982c0b8bcd72d7495?s=400';
+	function Person (firstName, lastName, birthday, imgUrl){
+		if (typeof(imgUrl)==='undefined') {
+			imgUrl = 'http://lorempixel.com/300/300/';
 		};
 
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.birthday = birthday;
-		this.imgPath = imgPath;
+		this.imgUrl = imgUrl;
 	};
 }
 
@@ -59,5 +59,16 @@ function BirthdayCalculator(){
 
 		return nextBday;
 	};
+
+	function thereIsaBirthdayToday(d1){
+              var d2 = new Date();
+                return d1.getUTCMonth() == d2.getUTCMonth() 
+                && d1.getUTCDate() == d2.getUTCDate();
+    };
+
+    this.thereIsaBirthdayToday = function(){
+    	var nextBDayPerson = this.getNextFromNow();
+    	return thereIsaBirthdayToday(nextBDayPerson.birthday);
+    }
 };
 
